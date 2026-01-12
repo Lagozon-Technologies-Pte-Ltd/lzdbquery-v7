dd6610b55b443dc0645b3a94f5baaf8541242224
simranLagozon <simran.kumari@Lagozon.com>
2026-01-12 17:34:05 +0530

    updated table details

--- table_details.py
+++ table_details.py
@@ -7,7 +7,7 @@ from pydantic import BaseModel, Field
 # from langchain_openai import ChatOpenAI 
 from openai import AzureOpenAI
 # from langchain_openai import AzureChatOpenAI
-import platform
+
 
 
 AZURE_OPENAI_API_KEY = os.environ.get('AZURE_OPENAI_API_KEY')
@@ -15,29 +15,9 @@ AZURE_OPENAI_ENDPOINT = os.environ.get('AZURE_OPENAI_ENDPOINT')
 AZURE_OPENAI_API_VERSION = os.environ.get('AZURE_OPENAI_API_VERSION', "2024-02-01")
 AZURE_DEPLOYMENT_NAME = os.environ.get('AZURE_DEPLOYMENT_NAME')
 
-# llm = AzureChatOpenAI(
-#     openai_api_version=AZURE_OPENAI_API_VERSION,
-#     azure_deployment=AZURE_DEPLOYMENT_NAME,
-#     azure_endpoint=AZURE_OPENAI_ENDPOINT,
-#     api_key=AZURE_OPENAI_API_KEY,
-#     temperature=0
-# )
+# 6
 
 from typing import List
-
-os_name = platform.system()
-if os_name == "Windows":
-    # Do something for Windows
-    print("Running on Windows")
-elif os_name == "Linux":
-    # Do something for Linux
-    print("Running on Linux")
-    __import__('pysqlite3')
-    import sys
-    sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
-else:
-    # Raise an exception for unsupported OS
-    raise RuntimeError(f"Unsupported OS: {os_name}")
 # __import__('pysqlite3')
 # import sys
 # sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
@@ -114,9 +94,7 @@ def get_table_details(table_name=None):
                 if is_fk: flags.append("FK")
                 if nullable: flags.append("NULLABLE")
                 flag_str = " [" + ", ".join(flags) + "]" if flags else ""
-                examples = col.get('examples', None)
-                example_str = f" Example: {examples}" if examples is not None else "no example for this column"
-                table_details += f"  - {col_name} ({data_type}){flag_str}: {example_str} {description} \n"
+                table_details += f"  - {col_name} ({data_type}){flag_str}: {description}\n"
         else:
             table_details += "  No column details found.\n"
         table_details += "\n"
